54d6dcb5c536a4aa6171445c1a170375cd085e54
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2017-10-18 20:10:23 -0700

    got the basic form created.  was able to capture all values except message.  next is to capture message, then to pass those values into nodemailer.

--- app/Scripts/app.js
+++ app/Scripts/app.js
@@ -1,6 +1,7 @@
 (function(module){
   let mail = {};
   mail.$recipient = null;
+  mail.$phone = null;
   mail.$email = null;
   mail.$message = null;
 
@@ -10,6 +11,14 @@
     console.log('clicked the submit');
     $('.contactForm').off().on('click', '.sendMail', function(e){
       e.preventDefault();
+      mail.$recipient = $('.contactForm input[name=sender]').val();
+      mail.$phone = $('.contactForm input[name=email]').val();
+      mail.$email = $('.contactForm input[name=phone]').val();
+      mail.$message = $('.contactForm input[name=message]').val();
+      console.log('recip', mail.$recipient);
+      console.log('phone', mail.$phone);
+      console.log('email', mail.$email);
+      console.log('msg', mail.$message);
     });
   };
 
@@ -33,7 +42,9 @@
 
 
 
-
+$(document).ready(function() {
+    mail.getForm();
+});
 
 
 
