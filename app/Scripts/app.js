85cbf789f5cce21d0ce6e9abc74bc1d61294e370
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2017-10-19 19:18:48 -0700

    got the email to work! however it is only sending the body of the message and not any of the other information.  that is next.

--- app/Scripts/app.js
+++ app/Scripts/app.js
@@ -1,6 +1,7 @@
 (function(module){
   let mail = {};
-  mail.$recipient = null;
+
+  mail.$sender = null;
   mail.$phone = null;
   mail.$email = null;
   mail.$message = null;
@@ -11,16 +12,29 @@
     console.log('clicked the submit');
     $('.contactForm').off().on('click', '.sendMail', function(e){
       e.preventDefault();
-      mail.$recipient = $('.contactForm input[name=sender]').val();
+      mail.$sender = $('.contactForm input[name=sender]').val();
       mail.$phone = $('.contactForm input[name=email]').val();
       mail.$email = $('.contactForm input[name=phone]').val();
-      mail.$message = $('.contactForm input[name=message]').val();
-      console.log('recip', mail.$recipient);
-      console.log('phone', mail.$phone);
-      console.log('email', mail.$email);
-      console.log('msg', mail.$message);
+      mail.$message = $('.contactForm textarea[name=message]').val();
+      $.get("http://localhost:9000/send", {
+        from: mail.$sender,
+        phone: mail.$phone,
+        email: mail.$email,
+        text: mail.$message
+      }, function(data){
+        if(data == "sent"){
+          console.log();
+          $('#sentMessage').empty().html(
+            'Your email has been sent.  Thank you!  We will be in touch soon.'
+          );
+        }
+      });
     });
   };
+
+  mail.sendForm = function(){
+    $()
+  }
 
 
 
